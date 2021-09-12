118c650c96bd8cc4cb0e2bffc76c225cc8957974
EduardoRam7 <edd_madrid@hotmail.com>
2021-09-12 18:00:29 -0500

    second commit to add the files

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+
+let now = moment();
+
+let year = now.get('year');
+let month = now.get('month');  // 0 to 11
+let date = now.get('date');
+let hour = now.get('hour');
+let minute = now.get('minute');
